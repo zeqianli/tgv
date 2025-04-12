69c0f69e5a92dbe105e399a62184819ac3f40091
Zeqian Li <zeqianli.bio@gmail.com>
2025-04-11 22:29:08 -0700

    add insta integration test

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use settings::{Cli, Settings};
 #[tokio::main]
 async fn main() -> Result<(), TGVError> {
     let cli = Cli::parse();
-    let settings: Settings = Settings::new(cli).unwrap();
+    let settings: Settings = Settings::new(cli, false).unwrap();
 
     let mut terminal = ratatui::init();
 
@@ -35,3 +35,25 @@ async fn main() -> Result<(), TGVError> {
 }
 
 // TODO: check .bai file
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use insta::{assert_snapshot, Settings as InstaSettings};
+    use ratatui::{backend::TestBackend, Terminal};
+    use std::env;
+    #[tokio::test]
+    async fn test_non_human_bam() {
+        let bam_path = env!("CARGO_MANIFEST_DIR").to_string() + "/tests/data/covid.sorted.bam";
+        let args_string = format!("tgv {} --no-reference", bam_path);
+
+        let cli = Cli::parse_from(shlex::split(&args_string).unwrap());
+        let settings = Settings::new(cli, true).unwrap();
+
+        let mut terminal = Terminal::new(TestBackend::new(50, 20)).unwrap();
+
+        let mut app = App::new(settings).await.unwrap();
+        app.run(&mut terminal).await.unwrap();
+
+        assert_snapshot!(terminal.backend());
+    }
+}
