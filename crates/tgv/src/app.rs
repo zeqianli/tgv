34ffa5e8992c81dcfd29c49c03d63ed19b7b8863
Zeqian Li <zeqianli.bio@gmail.com>
2025-12-25 12:57:30 -0800

    wip

--- crates/tgv/src/app.rs
+++ crates/tgv/src/app.rs
@@ -20,6 +20,13 @@ pub struct UIState {
     pub layout: MainLayout,
 }
 
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum Scene {
+    Main,
+    Help,
+    ContigList,
+}
+
 impl UIState {
     pub fn region(&self) -> Region {
         Region {
@@ -143,5 +150,107 @@ impl App {
     /// close connections
     pub async fn close(&mut self) -> Result<(), TGVError> {
         self.repository.close().await
+    }
+    /// Handle initial messages.
+    /// This has different error handling strategy (loud) vs handle(...), which suppresses errors.
+    pub async fn handle_initial_messages(
+        state: &mut State,
+        repository: &mut Repository,
+        registers: &mut Registers,
+        settings: &Settings,
+        messages: Vec<Message>,
+    ) -> Result<(), TGVError> {
+        let mut data_messages = Vec::new();
+
+        for message in messages {
+            data_messages.extend(
+                StateHandler::handle_state_message(state, repository, registers, settings, message)
+                    .await?,
+            );
+        }
+
+        let mut loaded_data = false;
+        for data_message in data_messages {
+            loaded_data = Self::handle_data_message(state, repository, data_message).await?;
+        }
+
+        Ok(())
+    }
+
+    /// Handle messages after initialization. This blocks any error messages instead of propagating them.
+    pub async fn handle(
+        state: &mut State,
+        repository: &mut Repository,
+        registers: &mut Registers,
+        settings: &Settings,
+        messages: Vec<Message>,
+    ) -> Result<(), TGVError> {
+        state.messages.clear();
+
+        let mut data_messages: Vec<DataMessage> = Vec::new();
+
+        for message in messages {
+            match StateHandler::handle_state_message(
+                state, repository, registers, settings, message,
+            )
+            .await
+            {
+                Ok(messages) => data_messages.extend(messages),
+                Err(e) => return StateHandler::add_message(state, e.to_string()),
+            }
+        }
+
+        let data_messages = StateHandler::get_data_requirements(state, repository)?;
+
+        for data_message in data_messages {
+            match Self::handle_data_message(state, repository, data_message).await {
+                Ok(_) => {}
+                Err(e) => return StateHandler::add_message(state, e.to_string()),
+            }
+        }
+
+        Ok(())
+    }
+
+    fn get_data_requirements(
+        state: &State,
+        repository: &mut Repository, // settings: &Settings,
+    ) -> Result<Vec<DataMessage>, TGVError> {
+        let mut data_messages = Vec::new();
+
+        // It's important to load sequence first!
+        // Alignment IO requires calculating mismatches with the reference sequence.
+
+        if repository.sequence_service.is_some()
+            && state.window.sequence_renderable()
+            && !state.sequence.has_complete_data(&viewing_region)
+        {
+            let sequence_cache_region = Self::sequence_cache_region(state, &viewing_region)?;
+            data_messages.push(DataMessage::RequiresCompleteSequences(
+                sequence_cache_region,
+            ));
+        }
+        if repository.alignment_repository.is_some()
+            && state.window.alignment_renderable()
+            && !state.alignment.has_complete_data(&viewing_region)
+        {
+            let alignment_cache_region = Self::alignment_cache_region(state, &viewing_region)?;
+            data_messages.push(DataMessage::RequiresCompleteAlignments(
+                alignment_cache_region,
+            ));
+        }
+
+        if repository.track_service.is_some() {
+            if !state.track.has_complete_data(&viewing_region) {
+                // viewing_window.zoom <= Self::MAX_ZOOM_TO_DISPLAY_FEATURES is always true
+                let track_cache_region = Self::track_cache_region(state, &viewing_region)?;
+                data_messages.push(DataMessage::RequiresCompleteFeatures(track_cache_region));
+            }
+
+            // Cytobands
+            data_messages.push(DataMessage::RequiresCytobands(state.contig_index()));
+        }
+
+        Ok(data_messages)
     }
 }
